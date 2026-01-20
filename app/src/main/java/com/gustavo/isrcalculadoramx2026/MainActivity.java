f4ebbe655f573d9dfab3544ff339abcb3224c9b9
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-01-20 12:37:57 -0600

    Crear ruta java y archivo MainActivity.java

---
+++ app/src/main/java/com/gustavo/isrcalculadoramx2026/MainActivity.java
@@ -0,0 +1,12 @@
+package com.gustavo.isrcalculadoramx2026;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
