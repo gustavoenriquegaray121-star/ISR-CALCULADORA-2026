ace298c9c6a1ff74af8611138e8efbc7f3513ae4
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-01-20 12:59:25 -0600

    Actualizar build.gradle.kts con configuración correcta para compilación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("com.android.application") version "7.4.2"
-    id("org.jetbrains.kotlin.android") version "1.7.20"
+    id("com.android.application")
 }
 
 android {
     namespace = "com.gustavo.isrcalculadoramx2026"
-    compileSdk = 33
-    buildToolsVersion = "33.0.0"
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gustavo.isrcalculadoramx2026"
-        minSdk = 23
-        targetSdk = 33
+        minSdk = 21
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -25,26 +25,17 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    // Usamos una versión más estable de MPAndroidChart
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
